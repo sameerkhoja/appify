b96545e0c1f474c684daa382cfc5231af04f97e8
Sameer Khoja <sameerkhoja10@gmail.com>
2016-01-18 23:39:52 -0600

    add existing project

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-inappbrowser": "1.1.1"
+}
+// BOTTOM OF METADATA
+});
